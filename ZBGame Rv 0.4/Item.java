c27cf8a26de3448aea2a9bc14c6876c86134a4f7
jseplin <jseplin@gmail.com>
2013-11-05 16:59:10 -0500

    V uploaded code
    Viola Uploaded Item

--- ZBGame Rv 0.4/Item.java
+++ ZBGame Rv 0.4/Item.java
@@ -1,42 +1,76 @@
 
- 
+package item;
+import java.util.ArrayList;
+//Revised Item Class
 
-/**
- *
- * @author Samantha
- */
-public class Item    //sbw eliminated inner class Item  2013 10 17  1600
-{
+/**With a backpack options. I did see the Inventory Class made. I just wanted
+* wasn't sure if we were going to use that for all items in general.
+* 
+* Viola DeShields
+*/
+
  
+    public class Item {
+  
+    public String name;    
     public int worth;
-    public int howFragile;
-    public double probabilityInRoom;
-    public Character health;
- 
-     public Item()  //sw: Armor extensd Items needs no-arg constructor  
-     {   }
+    public int dmg;
+    public double probablity; //I'm not sure if this is relevant right now
+    public final ItemType kindOf; //will get the item types
+    ArrayList<String> Backpack = new ArrayList<String>();//Will hold all items
+   
     
-    public Item(int w, int h, double p, Character hh)  //sw: hh //intializes the objects
-    {
-        probabilityInRoom = p;
-        howFragile = h;
-        worth = w;
-        health = hh;
-    }
-
-    public double getProb()
-    {
-        return probabilityInRoom;
-    }     
+   public Item(ItemType kindOf)
+   {
+       this.kindOf = kindOf;
+       this.Backpack= new ArrayList<String>();
+   }   
     
-    public int getFrag()
+    public int getDamage() //Getting the damage done from type of item
     {
-        return howFragile; //determines if it breaks easily
+       return kindOf.getDamageDone();
+    }    
+    
+    public boolean checkPack(String name) //Check to see if it's empty
+    {
+        if(Backpack.contains(name))
+        {
+            System.out.println("You have " + name + "in your backpack");
+            return true;
+            
+        }
+        return false;
     }
-    public int getWorth()
+    
+    public void addItem(String name, int dmg) //adding items to backpack
     {
-        return worth; //gets the worth of the items
+        Backpack.add(name);
+        System.out.println(name + " has been added to backpack.");
+    }
+    
+    public void removeItem(String name)//removing items from backpack
+    {
+        Backpack.remove(name);
+        System.out.println(name + " has been removed from backpack.");
+    }
+    
+    
+    
+    
+    
+    
+    public static void main(String[] args) 
+    {
+   //Test to see if my code was working
+        
+    ItemType swordType = new ItemType(null, 4); 
+    ItemType dagger = new ItemType(swordType, 3);
+    Item sword = new Item(swordType);
+    Item stuff = new Item(swordType);    
+    
+    
+    stuff.addItem("Fire", 45);          
+    stuff.removeItem("Fire");
+    
     }
 }
-    
-
